01d341a93361f3f26e3342a2f23707787e3967b2
alexandre <devnull@localhost>
2009-11-26 16:04:34 -0500

    [svn] added setup for deb packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="lunch",
+    version="0.1.2",
+    description="The Lunch Disctributed Process Manager",
+    author="SAT",
+    author_email="alexandre@sat.qc.ca",
+    url="http://svn.sat.qc.ca/trac/postures/wiki/Lunch",
+    packages=[],
+    scripts=["lunch"]
+    )
+
