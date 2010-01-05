a045a32a473e448a39e4690dbea63eeb0356b6e5
Alexandre Quessy <alexandre@quessy.net>
2010-01-05 10:48:05 -0500

    Fixed typo in setup.py. Added ssh-keyinstall bash script from http://svn.sat.qc.ca/trac/postures/browser/trunk/scripts/ssh-keyinstall at r31.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name="lunch",
     version="0.1.2",
-    description="The Lunch Disctributed Process Manager",
+    description="The Lunch Distributed Process Manager",
     author="SAT",
     author_email="alexandre@sat.qc.ca",
     url="http://svn.sat.qc.ca/trac/postures/wiki/Lunch",
