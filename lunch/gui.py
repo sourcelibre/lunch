6c72254ca2ec594625f8904e9df22cb1d2bedb79
Alexandre Quessy <alexandre@quessy.net>
2010-01-08 10:34:14 -0500

    added license in gui file

--- lunch/gui.py
+++ lunch/gui.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Lunch
+# Copyright (C) 2009 Société des arts technologiques (SAT)
+# http://www.sat.qc.ca
+# All rights reserved.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Lunch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Lunch.  If not, see <http://www.gnu.org/licenses/>.
+
 from twisted.internet import gtk2reactor
 gtk2reactor.install() # has to be done before importing reactor
 from twisted.internet import reactor
