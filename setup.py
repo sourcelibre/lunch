c91c027514adad5f835ad6dbb7728837f8d4af3f
Alexandre Quessy <alexandre@quessy.net>
2010-01-12 14:38:31 -0500

    Creating branch 0.1 Merged back to a single file.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name="lunch",
-    version="0.1.3",
+    version="0.1.6",
     description="The Lunch Distributed Process Manager",
     author="SAT",
     author_email="alexandre@quessy.net",
     url="https://svn.sat.qc.ca/trac/lunch",
-    packages=["lunch"],
+    packages=[],
     scripts=["scripts/lunch"]
     )
 
