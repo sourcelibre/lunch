1c8ddf8b33d25600aabfa9650ce4bf8b81ac8c1a
Alexandre Quessy <alexandre@quessy.net>
2010-01-13 15:03:57 -0500

    updated version number to 2.2 in files.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name="lunch",
-    version="0.2.0",
+    version="0.2.2",
     description="The Lunch Distributed Process Manager",
     author="SAT",
     author_email="alexandre@quessy.net",
     url="https://svn.sat.qc.ca/trac/lunch",
     packages=["lunch"],
-    scripts=["scripts/lunch", "scripts/lunch-0.1", "scripts/lunch-slave"]
+    scripts=["scripts/lunch", "scripts/lunch-slave"]
     )
 
