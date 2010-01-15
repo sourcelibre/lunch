0b9a4b58985af2cf1ca8edeabfb2a81631ca857a
Alexandre Quessy <alexandre@quessy.net>
2010-01-15 18:14:07 -0500

    Updated version number to 0.2.6 See changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="lunch",
-    version="0.2.5",
+    version="0.2.6",
     description="The Lunch Distributed Process Launcher",
     author="SAT",
     author_email="alexandre@quessy.net",
