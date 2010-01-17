f2acc3590f2b95bd441c781e3da3a776ff3334a8
Alexandre Quessy <alexandre@quessy.net>
2010-01-16 23:05:28 -0500

    Added menu bar and about dialog.

--- lunch/gui.py
+++ lunch/gui.py
@@ -27,6 +27,70 @@ from twisted.internet import defer
 import gtk
 import sys
 import os
+import webbrowser
+
+__version__ = "0.2.7"
+
+__license__ = """Lunch
+Copyright (C) 2009 Society for Arts and Technology (SAT)
+http://www.sat.qc.ca
+All rights reserved.
+
+This file is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Lunch is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lunch.  If not, see <http://www.gnu.org/licenses/>."""
+
+class About(object):
+    """
+    About dialog
+    """
+    def __init__(self):
+        # FIXME
+        self.icon_file = "/usr/share/pixmaps/lunch.png"
+        self.about_dialog = gtk.AboutDialog()
+
+    def show_about_dialog(self):
+        self.about_dialog.set_name('Lunch')
+        self.about_dialog.set_role('about')
+        self.about_dialog.set_version(__version__)
+        commentlabel = 'Simple Process Launcher for Complex Launching Setup.'
+        self.about_dialog.set_comments(commentlabel)
+        self.about_dialog.set_copyright("Copyright 2009 Society for Arts and Technology")
+        self.about_dialog.set_license(__license__)
+        self.about_dialog.set_authors([
+            'Alexandre Quessy <alexandre@quessy.net>', 
+            'Simon Piette <simonp@sat.qc.ca>'
+            ])
+        self.about_dialog.set_artists(['Rocket000'])
+        gtk.about_dialog_set_url_hook(self.show_website)
+        self.about_dialog.set_website("http://svn.sat.qc.ca/trac/lunch")
+        large_icon = gtk.gdk.pixbuf_new_from_file(self.icon_file)
+        self.about_dialog.set_logo(large_icon)
+        # Add button to show keybindings:
+        #shortcut_button = ui.button(text=_("_Shortcuts"))
+        #self.about_dialog.action_area.pack_start(shortcut_button)
+        #self.about_dialog.action_area.reorder_child(self.about_dialog.action_area.get_children()[-1], -2)
+        # Connect to callbacks
+        self.about_dialog.connect('response', self.destroy_about)
+        self.about_dialog.connect('delete_event', self.destroy_about)
+        #shortcut_button.connect('clicked', self.about_shortcuts)
+        self.about_dialog.connect("delete-event", self.destroy_about)
+        self.about_dialog.show_all()
+     
+    def show_website(self, widget, data):
+        webbrowser.open(data)
+
+    def destroy_about(self, *args):
+        self.about_dialog.destroy()
 
 class LunchApp(object):
     """
@@ -39,7 +103,7 @@ class LunchApp(object):
         # Window and framework
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_title("Lunch")
-        self.window.connect("destroy", self.destroy)
+        self.window.connect("destroy", self.destroy_app)
         self.window.set_default_size(320, 480)
         
         scroller = gtk.ScrolledWindow()
@@ -48,15 +112,15 @@ class LunchApp(object):
         self.commands = master.get_all_commands()
         num_rows = len(self.commands) + 1
         num_columns = 2
-        offset = 0
+        offset = 1
         self.table = gtk.Table(num_rows, num_columns, True)
         scroller.add_with_viewport(self.table)
         
-        #TODO:
-        #menubar = self.get_main_menu(self.window)
-        #self.table.attach(menubar, 0, 2, 0, 1)
+        # Menu bar
+        self.menubar = self.get_main_menu(self.window)
+        self.table.attach(self.menubar, 0, 2, 0, 1)
+        self.menubar.show()
         
-
         # Buttons
         self.title_labels = {}
         self.state_labels = {}
@@ -94,10 +158,10 @@ class LunchApp(object):
         #self.box1.pack_start(self.stopall_button, True, True, 0)
         #self.stopall_button.show()
 
-        self.quitbutton = gtk.Button("Quit")
-        self.quitbutton.connect("clicked", self.destroy)
-        self.table.attach(self.quitbutton, 0, 2, num_rows - 1, num_rows)
-        self.quitbutton.show()
+        #self.quitbutton = gtk.Button("Quit")
+        #self.quitbutton.connect("clicked", self.destroy)
+        #self.table.attach(self.quitbutton, 0, 2, num_rows - 1, num_rows)
+        #self.quitbutton.show()
         
         # Show the box
         self.table.show()
@@ -105,6 +169,43 @@ class LunchApp(object):
 
         # Show the window
         self.window.show()
+
+    def on_menu_open_logs(self, widget, data):
+        #TODO:
+        print "open logs"
+
+    def get_main_menu(self, window):
+        menu_items = (
+            ( "/_File", None, None, 0, "<Branch>" ),
+            #( "/File/_New", "<control>N", self.print_hello, 0, None),
+            ( "/File/_Open Logs", "<control>O", self.on_menu_open_logs, 0, None),
+            #( "/File/_Save", "<control>S", self.print_hello, 0, None),
+            #( "/File/Save _As", None, None, 0, None),
+            #( "/File/sep1", None, None, 0, "<Separator>"),
+            ( "/File/Quit", "<control>Q", self.destroy_app, 0, None),
+            #( "/_Options", None, None, 0, "<Branch>"),
+            #( "/Options/Test",  None, None, 0, None),
+            ( "/_Help", None, None, 0, "<LastBranch>"),
+            ( "/_Help/About", None, self.on_about, 0, None),
+            )
+        accel_group = gtk.AccelGroup()
+        item_factory = gtk.ItemFactory(gtk.MenuBar, "<main>", accel_group)
+        item_factory.create_items(menu_items)
+        window.add_accel_group(accel_group)
+        # need to keep a reference to item_factory to prevent its destruction
+        self.item_factory = item_factory
+        return item_factory.get_widget("<main>")
+
+    def on_about(self, widget, data):
+        print "on about"
+        About().show_about_dialog()
+    
+    def on_clicked(self, widget, data=None):
+        """
+        Our callback.
+        The data passed to this method is printed to stdout
+        """
+        print "Hello again - %s was pressed" % data
 
     def on_start_clicked(self, widget, info): # index as info
         #print "Button %s was pressed" % (info)
@@ -124,7 +225,7 @@ class LunchApp(object):
         self.state_labels[i].set_label(txt)
         print("GUI: Child %s changed its state to %s" % (command.identifier, new_state))
 
-    def destroy(self, widget, data=None):
+    def destroy_app(self, widget, data=None):
         """
         Destroy method causes appliaction to exit
         when main window closed
@@ -134,15 +235,6 @@ class LunchApp(object):
         print("reactor.stop()")
         reactor.stop()
 
-    def main(self):
-        """
-        All PyGTK applications need a main method - event loop
-        """
-        pass
-        #print("pre-gtk.main()")
-        #gtk.main()
-        #print("post-gtk.main()")
-
 def start_gui(lunch_master):
     """
     Starts the GTK GUI
@@ -151,7 +243,6 @@ def start_gui(lunch_master):
     print("Starting the GUI.")
     app = LunchApp(lunch_master)
     #self.slave_state_changed_signal = sig.Signal()
-    #reactor.callLater(0, app.main)
     return app
 
 if __name__ == "__main__":
