f4b618379c54aedd3dbf97894a10993fd1d170b4
Alexandre Quessy <alexandre@quessy.net>
2010-01-20 20:56:32 -0500

    updated version to 0.2.12

--- lunch/gui.py
+++ lunch/gui.py
@@ -33,7 +33,7 @@ import sys
 import os
 import webbrowser
 
-__version__ = "0.2.11"
+__version__ = "0.2.12"
 
 __license__ = """Lunch
 Copyright (C) 2009 Society for Arts and Technology (SAT)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="lunch",
-    version="0.2.11",
+    version="0.2.12",
     description="The Lunch Distributed Process Launcher",
     author="SAT",
     author_email="alexandre@quessy.net",
