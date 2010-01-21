7effea7918bf74e43b6b4534a10c53aa757eaf8e
Alexandre Quessy <alexandre@quessy.net>
2010-01-21 13:56:31 -0500

    able to modify cell in the treeview in prototype.

--- lunch/newtree.py
+++ lunch/newtree.py
@@ -14,80 +14,82 @@ class Example:
         return False
 
     def add_button_clicked(self, widget):
-        i0 = self.model_sort.get_model().append(
+        list_store = self.model_sort.get_model()
+        tree_iter_0 = list_store.append(
             [
                 "Milhouse_r_remote",
                 "milhouse -s --videoport 12309 --address 12.3.2.3 --videosource v4l2 --videocodec mpeg4 --videodevice /dev/video0",
                 "example.org",
-                4234.4334,
                 4,
                 "RUNNING"
             ])
         # select the new row
         selection = self.tree_view.get_selection()
-        i1 = self.model_sort.convert_child_iter_to_iter(None, i0)
-        selection.select_iter(i1)
+        tree_iter_1 = self.model_sort.convert_child_iter_to_iter(None, tree_iter_0)
+        selection.select_iter(tree_iter_1)
 
-    def add_row(self, command):
+    def add_row_in_tree(self, command):
+        """
+        adds a row with the data of the command.
+        """
+        list_store = self.model_sort.get_model()
+        list_store.append(self._format_command(command))
+        self.commands[command.identifier] = command
+
+    def _format_command(self, command):
+        """
+        Returns a list of values for the cells in the row of a command
+        """
         host = "localhost"
         if command.host is not None:
             host = command.host
-        life_time = 1.000 # TODO: remove
         executions = 2
         state = command.state
-        self.list_store.append(
-            [
-                command.title, 
+        return [
+                command.identifier, 
                 command.command,
                 host,
-                life_time, 
                 executions,
                 state
-            ])
-        
+            ]
 
-    def __init__(self):
+    ROW_IDENTIFIER = 0
 
-        # Create new window
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.set_title("Lunch Box")
-        self.window.set_size_request(220, 200)
-        self.window.connect("delete_event", self.delete_event)
-        self.vbox = gtk.VBox()
-        self.window.add(self.vbox)
-        self.scrollable = gtk.ScrolledWindow()
-        self.scrollable.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
-        self.scrollable.set_shadow_type(gtk.SHADOW_IN)
-        self.vbox.pack_start(self.scrollable, expand=True, fill=True)
+    def update_row(self, command):
+        list_store = self.model_sort.get_model()
+        looking_for = command.identifier
+        print "lllllllllllllopoook for:", looking_for
+        for row in iter(list_store):
+            identifier = row[self.ROW_IDENTIFIER]
+            if identifier == looking_for:
+                print identifier, "MATCHES!!!!!!!!!"
+                cells = self._format_command(command)
+                for i in range(len(cells)):
+                    row[i] = cells[i]
+                break
+            for v in row:
+                print v
         
-        # The ListStore contains the data.
-        self.list_store = gtk.ListStore(str, str, str, float, int, str)
-        # The TreeModelSort sorts the data
-        self.model_sort = gtk.TreeModelSort(self.list_store)
+    def _setup_treeview(self):
+        """
+        Needs attributes;
+         * self.tree_view
+         * self.model_sort
+        """
         # Set initial sorting column and order.
         sorting_column_number = 0
         self.model_sort.set_sort_column_id(sorting_column_number, gtk.SORT_ASCENDING)
-        # The TreeView displays the sorted data in the GUI.
-        self.tree_view = gtk.TreeView(self.model_sort)
-        
-        # Add button
-        self.add_button = gtk.Button('Add a Row')
-        self.add_button.connect('clicked', self.add_button_clicked)
-        self.vbox.pack_start(self.add_button, False)
-        # TreeView widget.
-        self.scrollable.add(self.tree_view)
-        
-        NUM_COLUMNS = 6
+
+        NUM_COLUMNS = 5
         columns = [None] * NUM_COLUMNS
         # Set column title
         columns[0] = gtk.TreeViewColumn("Title")
         columns[1] = gtk.TreeViewColumn("Command")
         columns[2] = gtk.TreeViewColumn("Host")
-        columns[3] = gtk.TreeViewColumn("Lifetime")
-        columns[4] = gtk.TreeViewColumn("Executions") # How many times
-        columns[5] = gtk.TreeViewColumn("State") # str
+        columns[3] = gtk.TreeViewColumn("Executions") # How many times
+        columns[4] = gtk.TreeViewColumn("State") # str
         
-        # Leave that as it is...
+        # Set default properties for each column
         cells = [None] * NUM_COLUMNS
         for i in range(NUM_COLUMNS):
             self.tree_view.append_column(columns[i])
@@ -97,10 +99,48 @@ class Example:
             columns[i].set_sort_column_id(i)
 
             cells[i] = gtk.CellRendererText()
-            cells[i].set_property("width-chars", 20) # FIXME
+            cells[i].set_property("width-chars", 20) 
             columns[i].pack_start(cells[i], False) #True)
             columns[i].set_attributes(cells[i], text=i)
-        # Done with the table. Showing the window.
+        # Set some custom properties
+        cells[0].set_property("width-chars", 14) # Title
+        cells[1].set_property("width-chars", 20) # Lifetime
+        cells[2].set_property("width-chars", 12) # host
+        cells[3].set_property("width-chars", 8) # Executions
+        cells[4].set_property("width-chars", 8) # State
+        #cells[4].set_property("foreground", 'green')
+
+
+    def __init__(self):
+        # Create new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("Lunch Box")
+        self.window.set_size_request(640, 480)
+        self.window.connect("delete_event", self.delete_event)
+        self.vbox = gtk.VBox()
+        self.window.add(self.vbox)
+        self.scrollable = gtk.ScrolledWindow()
+        self.scrollable.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+        self.scrollable.set_shadow_type(gtk.SHADOW_IN)
+        self.vbox.pack_start(self.scrollable, expand=True, fill=True)
+        
+        # The ListStore contains the data.
+        list_store = gtk.ListStore(str, str, str, int, str)
+        # The TreeModelSort sorts the data
+        self.model_sort = gtk.TreeModelSort(list_store)
+        # The TreeView displays the sorted data in the GUI.
+        self.tree_view = gtk.TreeView(self.model_sort)
+        self._setup_treeview()
+        self.scrollable.add(self.tree_view)
+        
+        self.commands = {} # for lunch
+
+        # Add button
+        self.add_button = gtk.Button('Add a Row')
+        self.add_button.connect('clicked', self.add_button_clicked)
+        self.vbox.pack_start(self.add_button, False)
+        
+        # Done. Showing the window.
         self.window.show_all()
 
 if __name__ == "__main__":
@@ -112,17 +152,20 @@ if __name__ == "__main__":
                 self.host = None
                 self.state = None
         obj = O()
-        obj.title = title
+        obj.identifier = title
         obj.command = command
         obj.host = host
         obj.state = state
         return obj
 
     example = Example()
-    example.add_row(factory('qweqweqwe', 'ls -lrt /'))
-    example.add_row(factory('milhouse', "milhouse -s --videoport 12309 --address 12.3.2.3 --videosource v4l2 --videocodec mpeg4 --videodevice /dev/video0"))
-    example.add_row(factory('milhouse-rekjfho9er=', 'milhouse -r '))
-    example.add_row(factory('qweqweqwe', 'ls -lrt /'))
-    example.add_row(factory('qweqweqwe', 'ls -lrt /'))
+    example.add_row_in_tree(factory('qweqweqwe', 'ls -lrt /'))
+    example.add_row_in_tree(factory('milhouse', "milhouse -s --videoport 12309 --address 12.3.2.3 --videosource v4l2 --videocodec mpeg4 --videodevice /dev/video0"))
+    example.add_row_in_tree(factory('milhouse-rekjfho9er=', 'milhouse -r '))
+    example.add_row_in_tree(factory('qweqweqwe', 'ls -lrt /'))
+    comm = factory('unique', 'ls -lrt /')
+    example.add_row_in_tree(comm)
 
+    comm.command = 'i changed'
+    reactor.callLater(1, example.update_row, comm)
     reactor.run()
