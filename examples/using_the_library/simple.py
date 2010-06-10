2b38681a3e9d8b7bf55c9dd09bf3660de4bd5ab0
Alexandre Quessy <alexandre@quessy.net>
2010-06-10 14:57:59 -0400

    The lunch master library example works

--- examples/using_the_library/simple.py
+++ examples/using_the_library/simple.py
@@ -3,6 +3,8 @@
 This example demonstrate how to use lunch master as a library.
 """
 from twisted.internet import reactor
+from twisted.internet import task
+from lunch import commands
 from lunch import master
 
 if __name__ == "__main__":
@@ -10,8 +12,13 @@ if __name__ == "__main__":
     log_dir = master.DEFAULT_LOG_DIR
     pid_file = master.write_master_pid_file(identifier=unique_master_id, directory=log_dir)
     # XXX add_command here
-    master.add_command("xeyes")
-    master.add_command("xlogo")
-    master.add_command("xcalc")
+    master.add_command("xeyes", title="xeyes")
+    master.add_command("xlogo", title="xlogo")
+    master.add_command("xcalc", title="xcalc")
     m = master.Master(log_dir=log_dir, pid_file=pid_file)
+    def _test():
+        print("Adding one more!")
+        m.add_command(commands.Command("xeyes"))
+    looping_call = task.LoopingCall(_test)
+    looping_call.start(1.0, False) 
     reactor.run()
