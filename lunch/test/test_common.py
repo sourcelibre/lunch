e46c403aa7c45af9b20d845989616b7233719023
Alexandre Quessy <alexandre@quessy.net>
2010-07-06 10:54:04 -0400

    Made the unit tests silent.

--- lunch/test/test_common.py
+++ lunch/test/test_common.py
@@ -8,7 +8,9 @@ from twisted.internet import reactor
 from lunch import master
 from lunch import commands
 
-master.start_stdout_logging("debug")
+master.start_stdout_logging("warning") # "debug"
+from lunch import logger
+log = logger.start(name="test")
 
 #TODO: add the path to lunch-slave to $PATH
 
@@ -29,21 +31,21 @@ class Test_Master(unittest.TestCase):
             # checks the the command has been added
             # removes the command
             self.the_command = _master.get_command(COMMAND_IDENTIFER)
-            print("Set self.the_command to %s" % (self.the_command))
+            log.info("Set self.the_command to %s" % (self.the_command))
             _master.remove_command(COMMAND_IDENTIFER)
-            print("remove_command")
+            log.info("remove_command")
             reactor.callLater(0.1, _later2)
         
         def _later2():
-            print("_later2")
+            log.info("_later2")
             # checks the the command has been removed
             if len(_master.get_all_commands()) != 0:
                 msg = "The command did not get removed"
-                print(msg)
+                log.info(msg)
                 _deferred.errback(failure.Failure(failure.DefaultException(msg)))
-                print("failed")
+                log.info("failed")
             else:
-                print("removing the looping call")
+                log.info("removing the looping call")
                 if _master._looping_call.running:
                     d = _master._looping_call.deferred
                     _master._looping_call.stop() # FIXME
@@ -53,7 +55,7 @@ class Test_Master(unittest.TestCase):
         
         def _cb3(result):
             # Called when the looping call has been stopped
-            print("quit all slaves")
+            log.info("quit all slaves")
             for command in _master.get_all_commands():
                 command.quit_slave()
             self.the_command.quit_slave()
@@ -64,19 +66,19 @@ class Test_Master(unittest.TestCase):
             
         
         _master.add_command(commands.Command(COMMAND_LINE, identifier=COMMAND_IDENTIFER))
-        print("added command $ %s" % (COMMAND_LINE))
+        log.info("added command $ %s" % (COMMAND_LINE))
         reactor.callLater(0.1, _later1)
         return _deferred
 
     #test_add_remove_command.skip = "This test is still not working."
         
-class Test_Command(unittest.TestCase):
-    def test_configure(self):
-        pass
-    def test_start(self):
-        pass
-    def test_stop(self):
-        pass
-    test_configure.skip = "TODO."
-    test_start.skip = "TODO."
-    test_stop.skip = "TODO."
+#class Test_Command(unittest.TestCase):
+#    def test_configure(self):
+#        pass
+#    def test_start(self):
+#        pass
+#    def test_stop(self):
+#        pass
+#    test_configure.skip = "TODO."
+#    test_start.skip = "TODO."
+#    test_stop.skip = "TODO."
